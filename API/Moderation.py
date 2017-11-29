e3e55d7bd925309ea5f0089c1a76c96ce08ee464
Michael Divis <mdivis@csumb.edu>
2017-11-29 11:37:04 -0800

    changed name to face_reader and added the API folder.

---
+++ API/Moderation.py
@@ -0,0 +1,146 @@
+# Team Project
+# Moderation.py
+# Group 205
+# CST 205
+# 11/22/17
+# Description: displays a webpage that will take image input and display all the images in the uploads folder
+# Python 3.6.0
+
+{
+    "status": "success",
+    "request": {
+        "id": "req_0ZbAUDYHHE3xIHDolfWU5",
+        "timestamp": 1497340963.0508,
+        "operations": 5
+    },
+    "weapon": 0.002,
+    "alcohol": 0.001,
+    "drugs": 0.001,
+    "scam": {
+        "prob": 0.1036
+    },
+    "nudity": {
+        "raw": 0.418388,
+        "partial": 0.469847,
+        "partial_tag": "chest",
+        "safe": 0.111764
+    },
+    "faces": [
+        {
+            "x1": 0.5527,
+            "y1": 0.226,
+            "x2": 0.6885,
+            "y2": 0.578,
+            "features": {
+                "left_eye": {
+                    "x": 0.6406,
+                    "y": 0.338
+                },
+                "right_eye": {
+                    "x": 0.5791,
+                    "y": 0.364
+                },
+                "nose_tip": {
+                    "x": 0.6045,
+                    "y": 0.392
+                },
+                "left_mouth_corner": {
+                    "x": 0.6367,
+                    "y": 0.476
+                },
+                "right_mouth_corner": {
+                    "x": 0.5908,
+                    "y": 0.492
+                }
+            },
+            "attributes": {
+                "female": 0.97,
+                "male": 0.03,
+                "minor": 0.06,
+                "sunglasses": 0.1
+            },
+            "celebrity": [
+                {
+                    "name": "Julia Voth",
+                    "prob": 0.09
+                },
+                {
+                    "name": "Adrianne Curry",
+                    "prob": 0.07
+                },
+                {
+                    "name": "Myriam Fares",
+                    "prob": 0.06
+                },
+                {
+                    "name": "Mila Kunis",
+                    "prob": 0.05
+                },
+                {
+                    "name": "Callie Hernandez",
+                    "prob": 0.05
+                }
+            ]
+        },
+        {
+            "x1": 0.2451,
+            "y1": 0.08,
+            "x2": 0.3799,
+            "y2": 0.466,
+            "features": {
+                "left_eye": {
+                    "x": 0.3418,
+                    "y": 0.25
+                },
+                "right_eye": {
+                    "x": 0.2803,
+                    "y": 0.226
+                },
+                "nose_tip": {
+                    "x": 0.2998,
+                    "y": 0.308
+                },
+                "left_mouth_corner": {
+                    "x": 0.3193,
+                    "y": 0.394
+                },
+                "right_mouth_corner": {
+                    "x": 0.2764,
+                    "y": 0.378
+                }
+            },
+            "attributes": {
+                "female": 0.01,
+                "male": 0.99,
+                "minor": 0.09,
+                "sunglasses": 0.04
+            },
+            "celebrity": [
+                {
+                    "name": "Justin Timberlake",
+                    "prob": 0.81
+                },
+                {
+                    "name": "Robert Webb",
+                    "prob": 0.11
+                },
+                {
+                    "name": "John Ales",
+                    "prob": 0.02
+                },
+                {
+                    "name": "David Fumero",
+                    "prob": 0.02
+                },
+                {
+                    "name": "Deborra-Lee Furness",
+                    "prob": 0.02
+                }
+            ]
+        }
+    ],
+    "media": {
+        "id": "med_0ZbATVy7TzQyygu8cegtD",
+        "uri": "https:\/\/sightengine.com\/assets\/img\/examples\/example5.jpg"
+    }
+}
---
+++ face_reader.py
@@ -0,0 +1,65 @@
+import os
+import urllib
+import numpy as np
+import cv2
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+from pprint import pprint
+
+
+def make_soup(url):
+    thepage = urllib.request.urlopen(url)
+    soupdata = BeautifulSoup(thepage, "html.parser")
+    return soupdata
+i=1
+soup = make_soup("https://csumb.edu/")
+for img in soup.findAll('img'):
+    temp=img.get('src')
+    if temp[:1]=="/":
+        image = "https://csumb.edu/" + temp
+    else:
+        image = temp
+
+    nametemp = img.get('alt')
+    if len(nametemp)==0:
+        filename=str(i)
+        i=i+1
+    else:
+        filename=nametemp
+
+
+    imagefile = open(filename + ".jpg", 'wb')
+    imagefile.write(urllib.request.urlopen(image).read())
+    imagefile.close()
+
+img = cv2.imread('1.jpg')
+
+print(type(img))
+
+print(img.shape)
+
+print(img.dtype)
+
+gray_img = cv2.imread('1.jpg', cv2.IMREAD_GRAYSCALE)
+
+print(gray_img.shape)
+
+key = cv2.waitKey()
+pic_remap = cv2.applyColorMap(gray_img, cv2.COLORMAP_WINTER)
+pic_remap1 = cv2.applyColorMap(gray_img, cv2.COLORMAP_JET)
+pic_remap2 = cv2.applyColorMap(gray_img, cv2.COLORMAP_BONE)
+while True:
+    cv2.imshow("Lab 13", img)
+    key = cv2.waitKey()
+    if ord('q') == key:
+        break
+    elif ord('g') == key:
+        img = cv2.imread('1.jpg', cv2.IMREAD_GRAYSCALE)
+    elif ord('h') == key:
+        img = pic_remap
+    elif ord('j') == key:
+        img = pic_remap1
+    elif ord('b') == key:
+        img = pic_remap2
+    elif ord('c') == key:
+        img = cv2.imread('1.jpg')
